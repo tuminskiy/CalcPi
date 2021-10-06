a8847d3be7ca84a26505eefd19247b5f72fd29d5
tuminskiy <unknowuzvername@gmail.com>
2021-10-07 00:43:08 +0300

    Added support calculating integral

--- main.cpp
+++ main.cpp
@@ -2,23 +2,47 @@
 #include <string_view>
 #include <random>
 #include <cmath>
+#include <algorithm>
+#include <iterator>
 
-auto create_random_generator(double min_value, double max_value) {
-  std::random_device rd;
-  std::mt19937 gen{ rd() };
+#define RUN_PI
+
+auto create_random_generator(unsigned int seed, double min_value, double max_value) {
+  std::mt19937 gen{ seed };
   std::uniform_real_distribution distrib{ min_value, max_value };
 
-  return [gen = std::move(gen), distrib = std::move(distrib)]() mutable {
-    return distrib(gen);
+  return [=]() mutable { return distrib(gen); };
+}
+
+auto create_circle_test(double x, double y, double r) {
+  std::random_device rd;
+
+  return [x_gen = create_random_generator(rd(), x - r, x + r),
+          y_gen = create_random_generator(rd(), y - r, y + r),
+          x, y, r_ = r*r]() mutable {
+
+    const double x_ = std::pow(x_gen() - x, 2);
+    const double y_ = std::pow(y_gen() - y, 2);
+
+    return x_ + y_ - r_ < std::numeric_limits<double>::epsilon();
   };
 }
 
+double func(double value) { return value*value*value + 1; }
+
+auto create_function_test(double a, double b) {
+  std::random_device rd;
+
+  return [x_gen = create_random_generator(rd(), a, b),
+          y_gen = create_random_generator(rd(), 0, func(b))]() mutable { return func(x_gen()) > y_gen(); };
+}
+
 template <class F>
-auto create_test_runner(std::size_t MAX_RUNS, F test_case) {
-  return [MAX_RUNS, test_case]() {
+auto create_test_runner(std::size_t max_runs, F& test_case) {
+  return [max_runs, &test_case = test_case]() mutable {
     std::size_t n_success = 0;
 
-    for (std::size_t i = 0; i < MAX_RUNS; ++i) {
+    for (std::size_t i = 0; i < max_runs; ++i) {
       if (test_case())
         ++n_success;
     }
@@ -27,8 +51,13 @@ auto create_test_runner(std::size_t MAX_RUNS, F test_case) {
   };
 }
 
-double caluclate_pi(std::size_t n_success, std::size_t max_runs) {
-  return 4.0 * n_success / static_cast<double>(max_runs);
+double calculate_pi(std::size_t n_success, std::size_t max_runs) {
+  return 4.0 * static_cast<double>(n_success) / static_cast<double>(max_runs);
+}
+
+double calculate_integral(double a, double b, std::size_t n_success, std::size_t max_runs) {
+  return static_cast<double>(n_success) / static_cast<double>(max_runs)
+    * (b - a) * func(b);
 }
 
 template <class T>
@@ -42,27 +71,103 @@ auto input(std::string_view msg) {
 }
 
 int main() {
+#ifdef RUN_PI
   const double x = input<double>("Input x: ");
   const double y = input<double>("Input y: ");
   const double r = input<double>("Input r: ");
+#else
+  const double a = input<double>("Input a: ");
+  const double b = input<double>("Input b: ");
+#endif
 
-  auto x_gen = create_random_generator(x - r, x + r);
-  auto y_gen = create_random_generator(y - r, y + r);
-
-  constexpr std::size_t MAX_RUNS = 10'000'000;
-
-  auto circle_test = [&x_gen = x_gen, &y_gen = y_gen, x, y, r_ = r*r]() {
-    const double x_ = std::pow(x_gen() - x, 2);
-    const double y_ = std::pow(y_gen() - y, 2);
-    
-    return x_ + y_ - r_ < std::numeric_limits<double>::epsilon();
+  const std::vector<std::size_t> MAX_RUNS = {
+    10'000,     // 10^4
+    100'000,    // 10^5
+    1'000'000,  // 10^6
+    10'000'000, // 10^7
+    100'000'000 // 10^8
   };
 
-  auto test_runner = create_test_runner(MAX_RUNS, circle_test);
+#ifdef RUN_PI
+  auto test = create_circle_test(x, y, r);
+#else
+  auto test = create_function_test(a, b);
+#endif
 
-  const std::size_t n_success = test_runner();
+  std::vector<std::vector<double>> series(MAX_RUNS.size());
 
-  std::cout << "Pi: " << caluclate_pi(n_success, MAX_RUNS);
+  for (int i = 0; i < MAX_RUNS.size(); ++i) {
+    std::size_t seria_index = 0;
+
+    for (const std::size_t max_runs : MAX_RUNS) {
+      auto test_runner = create_test_runner(max_runs, test);
+      const std::size_t n_success = test_runner();
+
+#ifdef RUN_PI
+      const double value = calculate_pi(n_success, max_runs);
+#else
+      const double value = calculate_integral(a, b, n_success, max_runs);
+#endif
+      series[seria_index++].push_back(value);
+    }
+  }
+
+  std::cout.precision(10);
+
+  for (std::size_t i = 0; i < MAX_RUNS.size(); ++i) {
+    std::cout << "Seria " << i + 1 << ": ";
+    std:copy(
+      std::cbegin(series[i]),
+      std::cend(series[i]),
+      std::ostream_iterator<double>{ std::cout, " "}
+    );
+    std::cout << '\n';
+  }
+
+  std::vector<std::vector<double>> epsilons;
+  epsilons.reserve(5);
+
+  for (const std::vector<double>& seria : series) {
+    std::vector<double> epsilon(MAX_RUNS.size());
+
+    std::transform(std::cbegin(seria), std::cend(seria), std::begin(epsilon),
+      [](double value) { return std::abs( (value - M_PI) / M_PI ); }
+    );
+
+    epsilons.push_back(std::move(epsilon));
+  }
+
+  for (std::size_t i = 0; i < MAX_RUNS.size(); ++i) {
+    std::cout << "Epsilon " << i + 1 << ": ";
+    std::copy(
+      std::cbegin(epsilons[i]),
+      std::cend(epsilons[i]),
+      std::ostream_iterator<double>{ std::cout, " "}
+    );
+    std::cout << '\n';
+  }
+
+
+  std::vector<double> mid_results;
+  mid_results.reserve(MAX_RUNS.size());
+
+  for (std::size_t run_index = 0; run_index < MAX_RUNS.size(); ++run_index) {
+    const double sum = std::accumulate(std::cbegin(series), std::cend(series), 0.0,
+      [run_index](double init, const std::vector<double>& seria) {
+        return init + seria[run_index];
+      }
+    );
+
+    mid_results.push_back(std::abs( ((sum / static_cast<double>(MAX_RUNS.size())) - M_PI) / M_PI ));
+  }
+
+  std::cout << "Mid results: ";
+  std::copy(
+    std::cbegin(mid_results),
+    std::cend(mid_results),
+    std::ostream_iterator<double>{ std::cout, " " }
+  );
+
 
   return 0;
 }
